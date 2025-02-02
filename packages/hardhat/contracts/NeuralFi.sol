e56f9a89f7ddb2a0c4812fecef20edd0b4b18693
Furkan Nabi Sumji <furkannabisumji@gmail.com>
2025-02-02 16:38:51 +0530

    added add and remove liquidity

--- packages/hardhat/contracts/NeuralFi.sol
+++ packages/hardhat/contracts/NeuralFi.sol
@@ -3,50 +3,108 @@ pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "./YieldAggregator.sol";
 
 contract NeuralFi is Ownable {
-
     struct Investment {
         address token;
         uint256 amount;
+        uint256 balance;
         uint256 timestamp;
     }
 
+    YieldAggregator aggregator;
+
+    mapping(address => bool) allowedTokens;
+
+    mapping(address => mapping(address => mapping(address => uint256))) public userLP;
+
+    struct UserLPs {
+        address tokenA;
+        address tokenB;
+    }
+
+    mapping(address => UserLPs[]) userLps;
+
     mapping(address => Investment[]) public userInvestments;
     address[] users;
-    IERC20 public stablecoin;
     address yield;
 
     event Invested(address indexed user, address token, uint256 amount, uint256 timestamp);
     event Withdrawn(address indexed user, address token, uint256 amount, uint256 timestamp);
 
-    constructor(address _stablecoin) Ownable(msg.sender) {
-        stablecoin = IERC20(_stablecoin);
+    constructor(address _aggregator) Ownable(msg.sender) {
+        aggregator = YieldAggregator(_aggregator);
     }
 
-    function setYield(address _yield) external onlyOwner{
-        require(yield==address(0), "Already set");
+    modifier onlyYield() {
+        require(yield == msg.sender, "Unauthorised");
+        _;
+    }
+    function setYield(address _yield) external onlyOwner {
+        require(yield == address(0), "Already set");
         yield = _yield;
     }
 
+    function setToken(address _token, bool _allow) external onlyOwner {
+        allowedTokens[_token] = _allow;
+    }
     function deposit(address token, uint256 amount) external {
         require(amount > 0, "Amount must be greater than zero");
-        IERC20(token).transferFrom(msg.sender, address(this), amount);
-        userInvestments[msg.sender].push(Investment(token, amount, block.timestamp));
+        require(allowedTokens[token] || token == address(0), "Invalid token");
+        IERC20(token).transferFrom(msg.sender, address(yield), amount);
+        userInvestments[msg.sender].push(Investment(token, amount, amount, block.timestamp));
         users.push(msg.sender);
         emit Invested(msg.sender, token, amount, block.timestamp);
     }
 
-    function withdraw(uint256 index) external {
-        Investment memory investment = userInvestments[msg.sender][index];
-        require(investment.amount > 0, "No investment found");
-        IERC20(investment.token).transfer(msg.sender, investment.amount);
-        delete userInvestments[msg.sender][index];
-        for(uint256 i = 0; i < users.length; i++) {
-            if(users[i]==msg.sender && userInvestments[msg.sender].length==0){
-                delete users[i];
+    function withdrawAll() external {
+        Investment[] memory investments = getInvestment(msg.sender);
+        for(uint256 k = 0; k<investments.length; k++){
+            aggregator.withdraw(msg.sender, investments[k].token, investments[k].balance);
+            emit Withdrawn(msg.sender, investments[k].token, investments[k].balance, block.timestamp);
+        }
+        delete userInvestments[msg.sender];
+        UserLPs[] memory lps = getLPs(msg.sender);
+        for(uint256 i = 0; i< lps.length; i++){
+        aggregator.removeLiquidity(msg.sender, lps[i].tokenA, lps[i].tokenB);
+        userLP[msg.sender][lps[i].tokenA][lps[i].tokenB] = 0;
+        delete userLps[msg.sender][i];
+        }
+    }
+
+    function newTx(
+        address user,
+        address tokenA,
+        address tokenB,
+        uint256 amountA,
+        uint256 amountB,
+        uint8 action
+    ) public onlyYield {
+        if (action == 0) {
+            userLP[user][tokenA][tokenB] += amountA;
+            userLps[user].push(UserLPs(tokenA, tokenB));
+            Investment[] memory investments = getInvestment(user);
+            for (uint256 i = 0; i < investments.length; i++) {
+                userInvestments[user][i].balance = 0;
             }
         }
-        emit Withdrawn(msg.sender, investment.token, investment.amount, block.timestamp);
+
+        if (action == 1) {
+            userInvestments[msg.sender].push(Investment(tokenA, amountA, amountA, block.timestamp));
+            userInvestments[msg.sender].push(Investment(tokenB, amountB, amountB, block.timestamp));
+        }
+    }
+
+    function getUsers() external view returns (address[] memory) {
+        return users;
+    }
+
+    function getInvestment(address user) public view returns (Investment[] memory) {
+        return userInvestments[user];
+    }
+
+    function getLPs(address user) public view returns (UserLPs[] memory) {
+        return userLps[user];
     }
 }
--- packages/hardhat/contracts/YieldAggregator.sol
+++ packages/hardhat/contracts/YieldAggregator.sol
@@ -5,56 +5,148 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./NeuralFi.sol";
 import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
-import {IPool} from "@aave/core-v3/contracts/interfaces/IPool.sol";
+import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
+import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
 
 contract YieldAggregator is Ownable {
     address public agent;
-    NeuralFi neuralfi;
+    NeuralFi public neuralfi;
     IUniswapV2Router02 public uniswapRouter;
-    IPool public aavePool;
 
-    constructor(
-        address _neuralfi,
-        address _agent,
-        address _uniswapRouter,
-        address _aavePool
-    ) Ownable(msg.sender) {
+    event LiquidityAdded(address indexed tokenA, address indexed tokenB, uint256 liquidity);
+    event LiquidityRemoved(address indexed user, address indexed tokenA, address indexed tokenB, uint256 liquidity);
+
+    constructor(address _neuralfi, address _agent, address _uniswapRouter) Ownable(msg.sender) {
         neuralfi = NeuralFi(_neuralfi);
         agent = _agent;
         uniswapRouter = IUniswapV2Router02(_uniswapRouter);
-        aavePool = IPool(_aavePool);
     }
 
-    modifier onlyAgent() {
-        require(msg.sender == agent, "Unauthorised");
+    modifier onlyAgentOrNeuralfi() {
+        require(msg.sender == agent || msg.sender == address(neuralfi), "Unauthorized");
         _;
     }
 
-    function swapOnUniswap(address _tokenIn, address _tokenOut, uint256 _amount) external onlyAgent {
+     modifier onlyNeuralfi() {
+        require(msg.sender == address(neuralfi), "Unauthorized");
+        _;
+    }
+
+    function addLiquidity(address tokenA, address tokenB) external onlyAgentOrNeuralfi {
+        uint256 totalAmountA;
+        uint256 totalAmountB;
+        address[] memory users = neuralfi.getUsers();
+        uint256[] memory userInvestments = new uint256[](users.length);
+        for (uint256 i = 0; i < users.length; i++) {
+            NeuralFi.Investment[] memory investments = neuralfi.getInvestment(users[i]);
+            for (uint256 j = 0; j < investments.length; j++) {
+                address userToken = investments[j].token;
+                uint256 amount = investments[j].balance;
+                if (userToken != tokenA && userToken != tokenB) {
+                    uint256 swappedAmountA = swapTokens(userToken, tokenA, amount / 2);
+                    uint256 swappedAmountB = swapTokens(userToken, tokenB, amount / 2);
+                    totalAmountA += swappedAmountA;
+                    totalAmountB += swappedAmountB;
+                    userInvestments[i] = swappedAmountA + swappedAmountB;
+                } else {
+                    totalAmountA += userToken == tokenA ? amount : 0;
+                    totalAmountB += userToken == tokenB ? amount : 0;
+                    userInvestments[i] = amount;
+                }
+            }
+
+            require(totalAmountA > 0 && totalAmountB > 0, "Insufficient funds");
+
+            IERC20(tokenA).approve(address(uniswapRouter), totalAmountA);
+            IERC20(tokenB).approve(address(uniswapRouter), totalAmountB);
+
+            address pair = getPair(tokenA, tokenB);
+            uint256 lpBefore = IERC20(pair).balanceOf(address(this));
+
+            (, , uint256 liquidity) = uniswapRouter.addLiquidity(
+                tokenA,
+                tokenB,
+                totalAmountA,
+                totalAmountB,
+                0,
+                0,
+                address(this),
+                block.timestamp
+            );
+
+            uint256 totalLP = IERC20(pair).balanceOf(address(this)) - lpBefore;
+
+            for (uint256 k = 0; k < users.length; k++) {
+                if (userInvestments[i] > 0) {
+                    neuralfi.newTx(
+                        users[k],
+                        tokenA,
+                        tokenB,
+                        (userInvestments[k] * totalLP) / (totalAmountA + totalAmountB),
+                        0,
+                        0
+                    );
+                }
+                emit LiquidityAdded(tokenA, tokenB, liquidity);
+            }
+        }
+    }
+
+    function removeLiquidity(address user, address tokenA, address tokenB) external onlyAgentOrNeuralfi {
+        address pair = getPair(tokenA, tokenB);
+        require(pair != address(0), "Invalid pair");
+
+        if (user == address(0)) {
+            address[] memory users = neuralfi.getUsers();
+            for (uint256 i = 0; i < users.length; i++) {
+                _removeLiquidity(users[i], address(this), tokenA, tokenB, pair);
+            }
+        } else {
+            _removeLiquidity(user, user, tokenA, tokenB, pair);
+        }
+    }
+    function _removeLiquidity(address user, address to, address tokenA, address tokenB, address pair) internal {
+        uint256 lpAmount = neuralfi.userLP(user, tokenA, tokenB);
+        require(lpAmount > 0, "No LP to remove");
+
+        IERC20(pair).approve(address(uniswapRouter), lpAmount);
+
+        (uint256 amountA, uint256 amountB) = uniswapRouter.removeLiquidity(
+            tokenA,
+            tokenB,
+            lpAmount,
+            0,
+            0,
+            to,
+            block.timestamp
+        );
+        if (to == address(this)) {
+            neuralfi.newTx(user, tokenA, tokenB, amountA, amountB, 1);
+        }
+        emit LiquidityRemoved(user, tokenA, tokenB, lpAmount);
+    }
+
+    function swapTokens(address _tokenIn, address _tokenOut, uint256 _amount) internal returns (uint256) {
         address[] memory path = new address[](2);
         path[0] = _tokenIn;
         path[1] = _tokenOut;
-        
+
         IERC20(_tokenIn).approve(address(uniswapRouter), _amount);
-        uniswapRouter.swapExactTokensForTokens(
+        uint256[] memory amounts = uniswapRouter.swapExactTokensForTokens(
             _amount,
             0,
             path,
             address(this),
             block.timestamp
         );
+
+        return amounts[amounts.length - 1];
     }
 
-     function addLiquidityOnUniswap(address tokenA, address tokenB) external onlyAgent {
-        uint amountADesired;
-        uint amountBDesired;
-        IERC20(tokenA).approve(address(uniswapRouter), amountADesired);
-        IERC20(tokenA).approve(address(uniswapRouter), amountBDesired);
-        uniswapRouter.addLiquidity(tokenA, tokenB, amountADesired, amountBDesired, 0, 0, address(neuralfi), block.timestamp);
+    function withdraw(address user, address token, uint256 amount) external onlyNeuralfi{
+    IERC20(token).transfer(user, amount);
     }
-
-    function depositToAave(address _token, uint256 _amount) external onlyAgent {
-        IERC20(_token).approve(address(aavePool), _amount);
-        aavePool.supply(_token, _amount, address(this), 0);
+    function getPair(address tokenA, address tokenB) internal view returns (address) {
+        return IUniswapV2Factory(uniswapRouter.factory()).getPair(tokenA, tokenB);
     }
 }
