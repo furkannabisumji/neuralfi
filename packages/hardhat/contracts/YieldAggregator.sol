b15b49737b524a1e9c0a5f8860708864002b2ae4
Furkan Nabi Sumji <furkannabisumji@gmail.com>
2025-02-03 11:53:41 +0530

    bug fixes

--- packages/hardhat/contracts/YieldAggregator.sol
+++ packages/hardhat/contracts/YieldAggregator.sol
@@ -9,11 +9,12 @@ import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
 import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
 
 contract YieldAggregator is Ownable {
-    address public agent;
     NeuralFi public neuralfi;
     IUniswapV2Router02 public uniswapRouter;
+    address public agent;
 
-    event LiquidityAdded(address indexed tokenA, address indexed tokenB, uint256 liquidity);
+
+    event LiquidityAdded(address indexed user, address indexed tokenA, address indexed tokenB, uint256 liquidity);
     event LiquidityRemoved(address indexed user, address indexed tokenA, address indexed tokenB, uint256 liquidity);
 
     constructor(address _neuralfi, address _agent, address _uniswapRouter) Ownable(msg.sender) {
@@ -22,77 +23,64 @@ contract YieldAggregator is Ownable {
         uniswapRouter = IUniswapV2Router02(_uniswapRouter);
     }
 
-    modifier onlyAgentOrNeuralfi() {
+    modifier onlyAuthorized() {
         require(msg.sender == agent || msg.sender == address(neuralfi), "Unauthorized");
         _;
     }
 
-     modifier onlyNeuralfi() {
-        require(msg.sender == address(neuralfi), "Unauthorized");
-        _;
-    }
-
-    function addLiquidity(address tokenA, address tokenB) external onlyAgentOrNeuralfi {
+    function addLiquidity(address tokenA, address tokenB) external onlyAuthorized {
+        address[] memory users = neuralfi.getUsers();
         uint256 totalAmountA;
         uint256 totalAmountB;
-        address[] memory users = neuralfi.getUsers();
-        uint256[] memory userInvestments = new uint256[](users.length);
+        uint256[] memory userLP = new uint256[](users.length);
+
         for (uint256 i = 0; i < users.length; i++) {
-            NeuralFi.Investment[] memory investments = neuralfi.getInvestment(users[i]);
-            for (uint256 j = 0; j < investments.length; j++) {
-                address userToken = investments[j].token;
-                uint256 amount = investments[j].balance;
-                if (userToken != tokenA && userToken != tokenB) {
-                    uint256 swappedAmountA = swapTokens(userToken, tokenA, amount / 2);
-                    uint256 swappedAmountB = swapTokens(userToken, tokenB, amount / 2);
-                    totalAmountA += swappedAmountA;
-                    totalAmountB += swappedAmountB;
-                    userInvestments[i] = swappedAmountA + swappedAmountB;
-                } else {
-                    totalAmountA += userToken == tokenA ? amount : 0;
-                    totalAmountB += userToken == tokenB ? amount : 0;
-                    userInvestments[i] = amount;
-                }
-            }
+         (uint256 investedA, uint256 investedB) = processUserInvestments(users[i], tokenA, tokenB);
+            totalAmountA += investedA;
+            totalAmountB += investedB;
+            userLP[i] = investedA + investedB;
+        }
 
-            require(totalAmountA > 0 && totalAmountB > 0, "Insufficient funds");
+        require(totalAmountA > 0 && totalAmountB > 0, "Insufficient funds");
+        IERC20(tokenA).approve(address(uniswapRouter), totalAmountA);
+        IERC20(tokenB).approve(address(uniswapRouter), totalAmountB);
 
-            IERC20(tokenA).approve(address(uniswapRouter), totalAmountA);
-            IERC20(tokenB).approve(address(uniswapRouter), totalAmountB);
+        address pair = getPair(tokenA, tokenB);
+        uint256 lpBefore = IERC20(pair).balanceOf(address(this));
+        (, , uint256 liquidity) = uniswapRouter.addLiquidity(
+            tokenA, tokenB, totalAmountA, totalAmountB, 0, 0, address(this), block.timestamp
+        );
+        uint256 totalLP = IERC20(pair).balanceOf(address(this)) - lpBefore;
 
-            address pair = getPair(tokenA, tokenB);
-            uint256 lpBefore = IERC20(pair).balanceOf(address(this));
-
-            (, , uint256 liquidity) = uniswapRouter.addLiquidity(
-                tokenA,
-                tokenB,
-                totalAmountA,
-                totalAmountB,
-                0,
-                0,
-                address(this),
-                block.timestamp
-            );
-
-            uint256 totalLP = IERC20(pair).balanceOf(address(this)) - lpBefore;
-
-            for (uint256 k = 0; k < users.length; k++) {
-                if (userInvestments[i] > 0) {
-                    neuralfi.newTx(
-                        users[k],
-                        tokenA,
-                        tokenB,
-                        (userInvestments[k] * totalLP) / (totalAmountA + totalAmountB),
-                        0,
-                        0
-                    );
-                }
-                emit LiquidityAdded(tokenA, tokenB, liquidity);
+        for (uint256 i = 0; i < users.length; i++) {
+            for(uint256 k = 0; k< userLP.length; k++) {
+                neuralfi.newTx(users[i], tokenA, tokenB, (userLP[k] * totalLP) / (totalAmountA + totalAmountB), 0, 0);
+                emit LiquidityAdded(users[i], tokenA, tokenB, liquidity);
             }
         }
     }
 
-    function removeLiquidity(address user, address tokenA, address tokenB) external onlyAgentOrNeuralfi {
+    function processUserInvestments(address user, address tokenA, address tokenB) internal returns (uint256, uint256) {
+        NeuralFi.Investment[] memory investments = neuralfi.getInvestment(user);
+        uint256 totalA;
+        uint256 totalB;
+
+        for (uint256 j = 0; j < investments.length; j++) {
+            uint256 amount = investments[j].balance;
+            if (investments[j].token != tokenA && investments[j].token != tokenB) {
+                uint256 swappedA = swapTokens(investments[j].token, tokenA, amount / 2);
+                uint256 swappedB = swapTokens(investments[j].token, tokenB, amount / 2);
+                totalA += swappedA;
+                totalB += swappedB;
+            } else {
+                totalA += investments[j].token == tokenA ? amount : 0;
+                totalB += investments[j].token == tokenB ? amount : 0;
+            }
+        }
+        return (totalA, totalB);
+    }
+
+    function removeLiquidity(address user, address tokenA, address tokenB) external onlyAuthorized {
         address pair = getPair(tokenA, tokenB);
         require(pair != address(0), "Invalid pair");
 
@@ -105,21 +93,16 @@ contract YieldAggregator is Ownable {
             _removeLiquidity(user, user, tokenA, tokenB, pair);
         }
     }
+
     function _removeLiquidity(address user, address to, address tokenA, address tokenB, address pair) internal {
         uint256 lpAmount = neuralfi.userLP(user, tokenA, tokenB);
         require(lpAmount > 0, "No LP to remove");
 
         IERC20(pair).approve(address(uniswapRouter), lpAmount);
-
         (uint256 amountA, uint256 amountB) = uniswapRouter.removeLiquidity(
-            tokenA,
-            tokenB,
-            lpAmount,
-            0,
-            0,
-            to,
-            block.timestamp
+            tokenA, tokenB, lpAmount, 0, 0, to, block.timestamp
         );
+
         if (to == address(this)) {
             neuralfi.newTx(user, tokenA, tokenB, amountA, amountB, 1);
         }
@@ -133,19 +116,15 @@ contract YieldAggregator is Ownable {
 
         IERC20(_tokenIn).approve(address(uniswapRouter), _amount);
         uint256[] memory amounts = uniswapRouter.swapExactTokensForTokens(
-            _amount,
-            0,
-            path,
-            address(this),
-            block.timestamp
+            _amount, 0, path, address(this), block.timestamp
         );
-
         return amounts[amounts.length - 1];
     }
 
-    function withdraw(address user, address token, uint256 amount) external onlyNeuralfi{
-    IERC20(token).transfer(user, amount);
+    function withdraw(address user, address token, uint256 amount) external onlyAuthorized {
+        IERC20(token).transfer(user, amount);
     }
+
     function getPair(address tokenA, address tokenB) internal view returns (address) {
         return IUniswapV2Factory(uniswapRouter.factory()).getPair(tokenA, tokenB);
     }
